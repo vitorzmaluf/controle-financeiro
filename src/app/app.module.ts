54b8e5ef81b8d22d0e135ba7e9a2d22e0b856047
Danilo Bizarria <danilobizarria@gmail.com>
2018-08-02 22:17:47 -0300

    automatic components imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GastosComponent } from './gastos/gastos.component';
+import { GastoInfoComponent } from './gasto-info/gasto-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GastosComponent,
+    GastoInfoComponent
   ],
   imports: [
     BrowserModule
