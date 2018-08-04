2228249b66bdb7b30e3fdd59cc2a3ee589320bbe
Danilo Bizarria <danilobizarria@gmail.com>
2018-08-03 21:33:53 -0300

    constructor function

--- src/app/models/gasto.model.ts
+++ src/app/models/gasto.model.ts
@@ -3,4 +3,16 @@ export class Gasto {
     name: String;
     price: String;
     date: Date;
+
+    constructor(
+        id: string = '',
+        name: string = '',
+        price: string = '',
+        date: Date = new Date(),
+    ) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.date = date;
+    }
 }
