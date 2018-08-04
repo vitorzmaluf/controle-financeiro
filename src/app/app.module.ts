5d7167dcc5d4698190977816fe907d3cf00411a5
Danilo Bizarria <danilobizarria@gmail.com>
2018-08-03 21:34:43 -0300

    added RoutingModule and HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GastosComponent } from './gastos/gastos.component';
 import { GastoInfoComponent } from './gasto-info/gasto-info.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     GastosComponent,
-    GastoInfoComponent
+    GastoInfoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
